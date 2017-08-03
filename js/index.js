63e470d84393fbb8dec9a7d4e38a7559d500a2e1
Willy Liu <arkazain@gmail.com>
2017-08-03 18:27:25 +0200

    ✅ Add animated header

---
+++ js/index.js
@@ -0,0 +1,40 @@
+main();
+function main() {
+  updateHeaderText();
+}
+
+
+/**
+ * updateHeaderText
+ * Updates the text in the header and makes it looks like it's written
+ * in real time sort of.
+ */
+function updateHeaderText() {
+  let texts = ["programming.", "cliché headers.", "web dev.", "a lot of stuff."];
+  let i = 0;
+  let headerInterval = setInterval(() => {
+    let caret = document.getElementById("header-caret");
+    caret.classList.remove("nodisplay");
+
+    if (i < texts.length) {
+      let text = document.getElementById("header-interest");
+      text.textContent = "";
+
+      let t = texts[i];
+      let j = 0;
+      let letterInterval = setInterval(() => {
+        if (j < t.length) {
+          text.textContent += t[j];
+          j++;
+        } else {
+          clearInterval(letterInterval);
+        }
+      }, 80);
+      i++;
+    } else {
+      // stop the interval once all texts have been shown
+      clearInterval(headerInterval);
+      caret.classList.add("nodisplay");
+    }
+  }, 3000);
+}
